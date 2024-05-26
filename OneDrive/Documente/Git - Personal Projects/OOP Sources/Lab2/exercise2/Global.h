540640489d65076de56382fedd01da6451bbb950
DenisaCazacu <161977403+DenisaCazacu@users.noreply.github.com>
2024-05-26 17:52:44 +0300

    Delete OneDrive/Documente/Git - Personal Projects/OOP Sources directory

--- OneDrive/Documente/Git - Personal Projects/OOP Sources/Lab2/exercise2/Global.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "StudentClass.h"
-
-int CompareMathGrade(StudentClass* student1, StudentClass* student2);
-int CompareName(StudentClass* student1, StudentClass* student2);
-int CompareEnglishGrade(StudentClass* student1, StudentClass* student2);
-int CompareHistoryGrade(StudentClass* student1, StudentClass* student2);
-int CompareAverageGrade(StudentClass* student1, StudentClass* student2);
